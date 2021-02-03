e8ca04dd52f84fff5e2bba6e8a4481b38e857842
Kazuma_M <km.km.kzkz00@gmail.com>
2021-02-03 17:31:12 +0900

    ブロックくっつける処理と綺麗にはめる処理
    フィールドにブロックをくっつける処理、ブロック管理配列準備と初期化処理、ブロックをフィールドにきれいにはめる処理

---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    // ブロックが配置されていればtrue、そうでなければfalse
+    bool[,] blocks = new bool[10, 20];
+
+    [SerializeField]GameObject scoreViewObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitBlocks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        var b = other.gameObject.transform.position;
+        var u = 0.1f;
+        var g = new Vector3(
+            (float)((int)(b.x / u)) * u,    //x座標
+            (float)((int)(b.y / u)) * u,    //y座標
+            transform.position.z        //z座標
+            );
+
+        //座標を反映
+        other.gameObject.transform.position = g;
+        //向きを一定にする
+        other.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        var rb = other.gameObject.GetComponent<Rigidbody>();
+        Destroy(rb);
+    }
+
+    void InitBlocks()
+    {
+        for (int i = 0; i < blocks.GetLength(0); i++)
+        {
+            for (int j = 0; j < blocks.GetLength(1); j++)
+            {
+                blocks[i, j] = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreView.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreView : MonoBehaviour
+{
+    [SerializeField]public TextMesh textMesh;
+
+    [SerializeField]int cnt = 0;
+    const int MAXCNT = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cnt++;
+        cnt %= 60;
+        if (cnt == 0)
+        {
+            Destroy(this.gameObject);
+        }
+        transform.position += Vector3.up * 0.05f;
+    }
+}
