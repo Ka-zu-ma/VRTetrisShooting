e6dbc95e427796072de56571e380de663280d93e
Kazuma_M <km.km.kzkz00@gmail.com>
2021-02-09 18:03:48 +0900

    配置されたブロックを把握する処理、ブロックユニットの作成
    配置されたブロックを把握する処理実装、ブロックユニットを作成

--- Assets/Scripts/BlockGenerator.cs
+++ Assets/Scripts/BlockGenerator.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct BlockType
+{
+    public int[,] shape;　//形状を定義する配列
+}
+
 public class BlockGenerator : MonoBehaviour
 {
     // ブロックのゲームオブジェクト
     [SerializeField]GameObject blockObj;
+
+    // ブロックタイプの作成
+    [SerializeField]BlockType[] blockTypes;
+
 
     // 左右どちらのコントローラかを取得するために使用するControllerクラス
     OVRInput.Controller controller;
@@ -15,6 +24,7 @@ public class BlockGenerator : MonoBehaviour
     {
         // 左右どちらのコントローラかを取得 
         controller = GetComponent<OVRControllerHelper>().m_controller;
+        CreateBlockType();
     }
 
     // Update is called once per frame
@@ -22,19 +32,85 @@ public class BlockGenerator : MonoBehaviour
     {//A/Xボタンを押したらブロックを発射
         if (OVRInput.GetDown(OVRInput.Button.One, controller))
         {
-            Fire(transform.position, transform.forward, blockObj);
+            Fire(transform.position, transform.forward, CreateBlock(Random.Range(0, 7)));
         }
-
     }
 
     //ブロックを発射する関数
     void Fire(Vector3 startPos, Vector3 direction, GameObject target)
     {
         //ブロックのコピーを生成
-        GameObject go = Instantiate(target);
+        //GameObject go = Instantiate(target);
         //ブロックの発射位置を設定
-        go.transform.position = startPos;
+        target.transform.position = startPos;
         //ブロックをコントローラ正面方向に放つ
-        go.GetComponent<Rigidbody>().AddForce(direction * 10f, ForceMode.Impulse);
+        target.GetComponent<Rigidbody>().AddForce(direction * 10f, ForceMode.Impulse);
+    }
+
+    // ブロックの種類を定義
+    void CreateBlockType()
+    {
+        blockTypes = new BlockType[7];
+        blockTypes[0].shape = new int[,] {
+            {0,0,0,0},
+            {0,0,0,0},
+            {1,1,1,1},
+            {0,0,0,0},
+        };
+        blockTypes[1].shape = new int[,] {
+            {1,1},
+            {1,1},
+        };
+        blockTypes[2].shape = new int[,] {
+            {0,1,0},
+            {1,1,1},
+            {0,0,0},
+        };
+        blockTypes[3].shape = new int[,] {
+            {0,0,1},
+            {1,1,1},
+            {0,0,0},
+        };
+        blockTypes[4].shape = new int[,] {
+            {1,0,0},
+            {1,1,1},
+            {0,0,0},
+        };
+        blockTypes[5].shape = new int[,] {
+            {1,1,0},
+            {0,1,1},
+            {0,0,0},
+        };
+        blockTypes[6].shape = new int[,] {
+            {0,1,1},
+            {1,1,0},
+            {0,0,0},
+        };
+    }
+
+    //ブロックを生成
+    GameObject CreateBlock(int typeNum)
+    {
+        int size = blockTypes[typeNum].shape.GetLength(0);
+
+        GameObject blockUnits = new GameObject("BlockUnits");
+        // 物理演算を適用させるためRigidbodyを追加
+        blockUnits.AddComponent<Rigidbody>();
+        // 二次元配列をループで処理
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                // ブロックを配置する位置であればブロックを生成
+                if (blockTypes[typeNum].shape[j, i] == 1)
+                {
+                    GameObject go = Instantiate(blockObj);
+                    go.transform.parent = blockUnits.transform;
+                    // ブロックを生成する位置は親オブジェクトの相対位置で決定
+                    go.transform.localPosition = new Vector3(i - size / 2, size / 2 - j, 0) * 0.1f;
+                }
+            }
+        }
+        return blockUnits;
     }
 }
--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -20,26 +20,6 @@ public class Field : MonoBehaviour
     {
         
     }
-
-    void OnCollisionEnter(Collision other)
-    {
-        var b = other.gameObject.transform.position;
-        var u = 0.1f;
-        var g = new Vector3(
-            (float)((int)(b.x / u)) * u,    //x座標
-            (float)((int)(b.y / u)) * u,    //y座標
-            transform.position.z        //z座標
-            );
-
-        //座標を反映
-        other.gameObject.transform.position = g;
-        //向きを一定にする
-        other.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-
-        var rb = other.gameObject.GetComponent<Rigidbody>();
-        Destroy(rb);
-    }
-
     void InitBlocks()
     {
         for (int i = 0; i < blocks.GetLength(0); i++)
@@ -49,5 +29,55 @@ public class Field : MonoBehaviour
                 blocks[i, j] = false;
             }
         }
+    }
+
+    int GetPosition(float value, float distance, int minRange, int maxRange)
+    {
+        for (int i = minRange; i < maxRange; i++)
+        {
+            if (Mathf.Abs(value - (float)(i)) < distance)
+            {
+                return i;
+            }
+        }
+        return minRange - 1;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        var b = other.gameObject.transform.position;
+        var u = 0.1f;
+        var g = new Vector3(
+            ((int)(b.x / u)) * u,           //x座標
+            ((int)(b.y / u)) * u,           //y座標
+            transform.position.z        //z座標
+            );
+
+        //向きを一定にする
+        other.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        //左下座標を定義
+        float px = -0.4f;
+        float py = 0.1f;
+
+        //blocksの要素番号を取得
+        int bx = GetPosition((g.x - px) / u, 0.01f, 0, 10);
+        int by = GetPosition((g.y - py) / u, 0.01f, 0, 20);
+
+        //配置された位置の配列をtrueにする
+        blocks[bx, by] = true;
+
+        //参照できるように名前を設定
+        other.gameObject.name = $"name:{bx},{by}";
+        //座標を反映
+        other.gameObject.transform.position = new Vector3(bx * u + px, by * u + py, transform.position.z);
+
+        var rb = other.gameObject.GetComponent<Rigidbody>();
+        Destroy(rb);
+
+        // 位置を表示
+        GameObject sv = Instantiate(scoreViewObj);
+        sv.transform.position = other.gameObject.transform.position;
+        sv.GetComponent<ScoreView>().textMesh.text = $"({bx},{by})";
     }
 }
